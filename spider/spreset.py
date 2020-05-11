b542590c4519b575b3de3fdeaa79ee0677681b8d
arcturus5340 <arcturus5340@gmail.com>
2020-05-11 18:19:36 +0200

    Change the script spreset.py function
    Now the script spreset.py clears the fields of the database
    table. The refusal to clear all database tables is caused by the
    fact that the previous function was not necessary

--- spider/spreset.py
+++ spider/spreset.py
@@ -4,9 +4,8 @@ import sqlite3
 conn = sqlite3.connect('spider.sqlite')
 cur = conn.cursor()
 
-cur.execute('DROP TABLE IF EXISTS Pages')
-cur.execute('DROP TABLE IF EXISTS Links')
-cur.execute('DROP TABLE IF EXISTS Webs')
+cur.execute('UPDATE Pages SET old_rank=0.0, new_rank=1.0')
+print('All pages set to a rank of 1.0')
 
 conn.commit()
 conn.close()
