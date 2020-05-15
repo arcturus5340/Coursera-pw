8b7dc1a422c91cc781b8caa972ef511028e09a11
arcturus5340 <arcturus5340@gmail.com>
2020-05-15 21:39:39 +0200

    Add spjson.py
    Added a script to transfer data from the database to json for
    further use when visualizing data on the web

---
+++ spider/spjson.py
@@ -0,0 +1,30 @@
+import sqlite3
+import random
+
+
+conn = sqlite3.connect('spider.sqlite')
+cur = conn.cursor()
+
+cur.execute('SELECT COUNT(rank) FROM Pages WHERE rank ISNULL')
+if cur.fetchone()[0] != 0:
+    print('Start sprank.py first')
+    quit()
+
+cur.execute('SELECT COUNT(from_id) AS weight, rank, id, url FROM Pages JOIN Links ON id = from_id GROUP BY id')
+
+fhandle = open('spider.js', 'w')
+# print('spiderJSON = {"nodes":[', file=fhandle)
+print('spiderJson = {"nodes":[', file=fhandle)
+for row in cur:
+    row = (row[0], random.random(), row[2], row[3])
+    print('{{"weight": {}, "rank": {}, "id": {}, "url": "{}"}},'.format(*row), file=fhandle)
+print('],', file=fhandle)
+
+cur.execute('SELECT from_id, to_id FROM Links')
+
+print('"links":[', file=fhandle)
+for row in cur:
+    print('{{"source": {}, "target": {}, "value": 1 }},'.format(*row), file=fhandle)
+print(']};', file=fhandle)
+
+conn.close()
