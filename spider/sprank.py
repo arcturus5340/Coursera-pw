ed3a2818bc353b3e6505af8ca5252f2f7fd350b9
arcturus5340 <arcturus5340@gmail.com>
2020-05-18 17:01:39 +0200

    Improve page ranking
    Users are requested the number of times the PR-algorithm is
    applied to the list of web pages. This is necessary to adjust
    the accuracy of the rank, and also contributed to the rejection
    of the normalization of output data (rank)

--- spider/sprank.py
+++ spider/sprank.py
@@ -7,12 +7,12 @@ cur = conn.cursor()
 cur.execute('SELECT COUNT(*) FROM Pages')
 elem_count = cur.fetchone()[0]
 
-cur.execute('SELECT id FROM Pages')
+cur.execute('SELECT id, rank FROM Pages')
 rating = dict()
 new_rating = dict()
-for id in cur:
-    rating[id[0]] = 1/elem_count
-    new_rating[id[0]] = 0.0
+for id, rank in cur:
+    rating[id] = rank or (1/elem_count)
+    new_rating[id] = 0.0
 
 cur.execute('SELECT from_id, to_id FROM Links')
 links = dict()
@@ -21,18 +21,20 @@ for from_id, to_id in cur:
     val.append(to_id)
     links[int(from_id)] = val
 
-for node in rating:
-    incoming_edges = []
-    for link in links:
-        if node in links[link]: incoming_edges.append(link)
+cycle_count = int(input('Enter the number of cycles to calculate ranks: '))
+for i in range(cycle_count):
+    for node in rating:
+        incoming_edges = []
+        for link in links:
+            if node in links[link]: incoming_edges.append(link)
 
-    r = (1-0.85)/elem_count + 0.85*sum(rating[edge]/len(links[edge]) for edge in incoming_edges)
-    new_rating[node] = r
-
-new_rating = {node: r/sum(new_rating.values()) for node, r in new_rating.items()}
+        r = (1-0.85)/elem_count + 0.85*sum(rating[edge]/len(links[edge]) for edge in incoming_edges)
+        new_rating[node] = r
+    rating = new_rating.copy()
 
 for node, r in new_rating.items():
     cur.execute('UPDATE Pages SET rank = ? WHERE id = ?', (r, node))
 
+print('Done. Run spjson.py')
 conn.commit()
 cur.close()
