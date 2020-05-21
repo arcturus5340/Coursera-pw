a4da0ffcf9912bc1b7f2d4b462a6eb1a5faf52ab
arcturus5340 <arcturus5340@gmail.com>
2020-05-21 23:18:19 +0200

    Change the database structure
    The database structure has been changed to reduce memory
    consumption. The recipient of emails as well as names are
    no longer stored in the database. For future visualization,
    this data is redundant.
    
    Also, server names (domain names of mail servers) are now
    stored in a separate table to save memory

--- gmane/gmane.py
+++ gmane/gmane.py
@@ -8,28 +8,14 @@ conn = sqlite3.connect('content.sqlite')
 cur = conn.cursor()
 cur.execute('CREATE TABLE IF NOT EXISTS Messages ('
             'id INTEGER PRIMARY KEY AUTOINCREMENT,'
-            'sender_server TEXT,'
-            'sender_name TEXT,'
-            'sender_username TEXT,'
-            'recipient_server TEXT,'
-            'recipient_name TEXT,'
-            'recipient_username TEXT,'
+            'sender_id INTEGER,'
             'sent_at TEXT,'
             'subject TEXT,'
             'body TEXT)')
 
-cur.execute('CREATE TABLE IF NOT EXISTS Senders ('
-            'id INTEGER PRIMARY KEY AUTOINCREMENT,'
-            'name TEXT,'
-            'username TEXT,'
-            'server TEXT)')
-
-cur.execute('CREATE TABLE IF NOT EXISTS Recipients ('
-            'id INTEGER PRIMARY KEY AUTOINCREMENT,'
-            'name TEXT,'
-            'username TEXT,'
-            'server TEXT)')
-
+cur.execute('CREATE TABLE IF NOT EXISTS Servers ('
+            'id INTEGER PRIMARY KEY,'
+            'dns TEXT)')
 
 cur.execute('SELECT MAX(id) FROM Messages')
 start = cur.fetchone()[0]
@@ -39,19 +25,19 @@ else:
     start = 1
 
 message_data_list = []
+dns_list = []
 count = int(input('How many messages? '))
 for i in range(start, start+count):
-    message_data = {'sender': None, 'recipient': None, 'sent_at': None, 'subject': None}
+    message_data = {'sender': None, 'sent_at': None, 'subject': None, 'body': None}
     url = 'http://mbox.dr-chuck.net/sakai.devel/{}/{}'.format(i, i+1)
     html = urllib.request.urlopen(url).read().decode()
-    email = re.findall('\\nFrom: (.*?) <(\S+)@(\S+)>\\n', html)
+    email = re.findall('\\nFrom: .*? <\S+@(\S+)>\\n', html)
     if email:
-        data = [field.strip('"') for field in email[0]]
-        message_data['sender'] = data
-    email = re.findall('\\nTo: (.*?) <(\S+)@(\S+)>\\n', html)
-    if email:
-        data = [field.strip('"') for field in email[0]]
-        message_data['recipient'] = data
+        dns = email[0]
+        if dns not in dns_list:
+            dns_list.append(dns)
+        id = dns_list.index(dns)
+        message_data['sender'] = id
 
     date = re.findall('\\nDate: (.*?)[\\n(]', html)[0]
     pdate = dateutil.parser.parse(date)
@@ -59,10 +45,21 @@ for i in range(start, start+count):
 
     message_data['subject'] = re.findall('\nSubject:(?:\sR[eE]:)*\s(.*)\n', html)[0]
 
-    message_data['body'] = html.split('\n\n', 1)[1]
+    body = html.split('\n\n', 1)[1]
+    body = re.sub('[^a-zA-Z]+', ' ', body)
+    message_data['body'] = body
 
     message_data_list.append(message_data)
     conn.commit()
 
-print(message_data_list)
+for message in message_data_list:
+    cur.execute('INSERT INTO Messages(sender_id, sent_at, subject, body) '
+                'VALUES (?, ?, ?, ?)', (list(message.values())))
+    conn.commit()
+
+for i, dns in enumerate(dns_list):
+    cur.execute('INSERT INTO Servers(id, dns) '
+                'VALUES (?, ?)', (i, dns))
+    conn.commit()
+
 conn.close()
