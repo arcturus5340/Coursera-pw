846216515a603aeb6002111fabded0c18be7d224
arcturus5340 <arcturus5340@gmail.com>
2020-05-23 21:37:42 +0200

    Add the ability to download emails in stages
    Now the user can set the number of messages for downloading
    by updating the database each time the script gmane.py is run

--- gmane/gmane.py
+++ gmane/gmane.py
@@ -15,7 +15,7 @@ cur.execute('CREATE TABLE IF NOT EXISTS Messages ('
 
 cur.execute('CREATE TABLE IF NOT EXISTS Servers ('
             'id INTEGER PRIMARY KEY,'
-            'dns TEXT)')
+            'dns TEXT UNIQUE)')
 
 cur.execute('SELECT MAX(id) FROM Messages')
 start = cur.fetchone()[0]
@@ -25,13 +25,19 @@ else:
     start = 1
 
 message_data_list = []
-dns_list = []
+
+cur.execute('SELECT dns FROM Servers ORDER BY id')
+dns_list = [x[0] for x in cur]
+dns_count = len(dns_list)
 count = int(input('How many messages? '))
 for i in range(start, start+count):
+    if i == 60421:
+        print('All emails are downloaded!')
+        break
     message_data = {'sender': None, 'sent_at': None, 'subject': None, 'body': None}
     url = 'http://mbox.dr-chuck.net/sakai.devel/{}/{}'.format(i, i+1)
     html = urllib.request.urlopen(url).read().decode()
-    email = re.findall('\\nFrom: .*? <\S+@(\S+)>\\n', html)
+    email = re.findall('\nFrom:\s.*?<?\S+@(\S+?)>?\n', html)
     if email:
         dns = email[0]
         if dns not in dns_list:
@@ -39,9 +45,12 @@ for i in range(start, start+count):
         id = dns_list.index(dns)
         message_data['sender'] = id
 
-    date = re.findall('\\nDate: (.*?)[\\n(]', html)[0]
+    date = re.findall('\\nDate:\s(.*?)[\\n(]', html)[0]
     pdate = dateutil.parser.parse(date)
-    message_data['sent_at'] = pdate.isoformat()[:7]
+    try:
+        message_data['sent_at'] = pdate.isoformat()[:7]
+    except dateutil.parser.UnknownTimezoneWarning:
+        continue
 
     message_data['subject'] = re.findall('\nSubject:(?:\sR[eE]:)*\s(.*)\n', html)[0]
 
@@ -50,15 +59,21 @@ for i in range(start, start+count):
     message_data['body'] = body
 
     message_data_list.append(message_data)
-    conn.commit()
+
+    if i % 100 == 0:
+        for message in message_data_list:
+            cur.execute('INSERT INTO Messages(sender_id, sent_at, subject, body) '
+                        'VALUES (?, ?, ?, ?)', (list(message.values())))
+        conn.commit()
+        message_data_list= []
 
 for message in message_data_list:
     cur.execute('INSERT INTO Messages(sender_id, sent_at, subject, body) '
                 'VALUES (?, ?, ?, ?)', (list(message.values())))
     conn.commit()
 
-for i, dns in enumerate(dns_list):
-    cur.execute('INSERT INTO Servers(id, dns) '
+for i, dns in enumerate(dns_list[dns_count:], dns_count):
+    cur.execute('INSERT OR IGNORE INTO Servers(id, dns) '
                 'VALUES (?, ?)', (i, dns))
     conn.commit()
 
