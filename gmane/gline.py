75b2b421d7c5c3067e3c6a22966fc2e43bda9246
arcturus5340 <arcturus5340@gmail.com>
2020-05-24 22:15:17 +0200

    Fix the error with the analysis of mail server domains
    Fixed a bug providing a list of the most popular mail
    servers. Previously, values were sorted by server name. Now
    according to the number of emails that came from them

--- gmane/gline.py
+++ gmane/gline.py
@@ -7,14 +7,14 @@ cur = conn.cursor()
 cur.execute('SELECT dns, sent_at FROM Messages JOIN Servers ON Messages.sender_id = Servers.id')
 
 date_dict = {}
-dns_list = []
+dns_dict = {}
 for dns, date in cur:
-    if dns not in dns_list: dns_list.append(dns)
+    dns_dict[dns] = dns_dict.get(dns, 0) + 1
     dns_dict = date_dict.get(date, dict())
     dns_dict[dns] = dns_dict.get(dns, 0) + 1
     date_dict[date] = dns_dict
 
-dns_list = sorted(dns_list, reverse=True)[:10]
+dns_list = sorted(dns_dict.keys(), key=dns_dict.get, reverse=True)[:10]
 fhandle = open('gline.js', 'w')
 print('gline = [', file=fhandle)
 print('[ "Year",', ', '.join(list('"'+x+'"' for x in dns_list)), '],', file=fhandle)
