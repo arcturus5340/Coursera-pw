5d9075242737a865f990a5cea6fa2ad60dfffb8c
arcturus5340 <arcturus5340@gmail.com>
2020-05-25 23:51:46 +0200

    Add a search for other dna sequences
    Now the getNextGene() function finds other DNA triplets
    that terminate the synthesis of the polypeptide chain
    (translation), namely, the sequences “TAG” and “TGA”

--- genefinder/GeneFinder.java
+++ genefinder/GeneFinder.java
@@ -1,38 +1,64 @@
 package genefinder;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class GeneFinder {
-    private String data;
+    private String dna;
     private int currentPosition = 0;
 
     public GeneFinder(String filename) {
         FileResource file = new FileResource("src/genefinder/src/"+filename);
-        String data = file.getData();
-        this.data = data;
+        this.dna = file.getData();
     }
 
-    public String getNextGene() {
-        int startPosition = this.data.indexOf("atg", this.currentPosition);
+    public List<String> getAllGenes() {
+        List<String> geneStorage = new ArrayList<String>();
+        String gene = this.getNextGene();
+        while (!gene.equals("")) {
+            geneStorage.add(gene);
+            gene = this.getNextGene();
+        }
+        return geneStorage;
+    }
+
+    private int findStopCodon(String codon, int position) {
         int finishPosition;
-        boolean found = false;
+        int tmpPosition = position;
         while (true) {
-            startPosition = this.data.indexOf("atg", startPosition + 1);
-            if (startPosition == -1) {
-                this.currentPosition = -1;
-                return "";
-            }
-            int tmpPosition = startPosition;
-            while(true) {
-                finishPosition = this.data.indexOf("taa", tmpPosition + 1);
-                if (finishPosition == -1) {
-                    break;
-                } else if ((finishPosition - startPosition) % 3 == 0) {
-                    this.currentPosition = finishPosition;
-                    return this.data.substring(startPosition, finishPosition+3);
-                } else {
-                    tmpPosition = finishPosition;
-                }
+            finishPosition = this.dna.indexOf(codon, tmpPosition + 1);
+            if (finishPosition == -1) {
+                return this.dna.length();
+            } else if ((finishPosition - position) % 3 == 0) {
+                return finishPosition + 3;
+            } else {
+                tmpPosition = finishPosition;
             }
         }
     }
+
+    public String getNextGene() {
+        int startPosition = this.dna.indexOf("atg", this.currentPosition);
+        while (true) {
+            if (startPosition == -1) {
+                this.currentPosition = 0;
+                return "";
+            }
+            int taa_pos = findStopCodon("taa", startPosition);
+            int tag_pos = findStopCodon("tag", startPosition);
+            int tga_pos = findStopCodon("tga", startPosition);
+            int min_pos = Math.min(Math.min(taa_pos, tag_pos), tga_pos);
+            if (min_pos != this.dna.length()) {
+                this.currentPosition = min_pos;
+                return this.dna.substring(startPosition, min_pos);
+            }
+            startPosition = this.dna.indexOf("atg", startPosition + 1);
+        }
+    }
+
 }
 
